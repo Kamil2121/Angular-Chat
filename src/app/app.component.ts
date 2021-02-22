586f79d1baf242ec1efe9d7547cb20c66b9d3849
Kamil Świercz <kamilswiercz@MacBook-Air-Kamil.local>
2021-02-22 22:07:29 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,15 @@ export class AppComponent {
   chatPanel = false;
   loginPanel = true;
   nickname: string;
+  reg1 = new RegExp('\\s'); // białe znaki
+  reg4: boolean; // pusta wiadomość
 
   ConnectChat(): void {
-    this.loginPanel = false;
-    this.chatPanel = true;
+    if (this.nickname === '') { this.reg4 = true; } else { this.reg4 = false; }
+    if (this.reg1.test(this.nickname) || this.reg4) {}
+    else {
+      this.loginPanel = false;
+      this.chatPanel = true;
+    }
   }
 }
--- src/app/chat-box/chat-box.component.ts
+++ src/app/chat-box/chat-box.component.ts
@@ -1,26 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { io } from 'socket.io-client';
 
 const SOCKET_ENDPOINT = 'https://mychat-angular.herokuapp.com';
-// 'localhost:8080';
 
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
   styleUrls: ['./chat-box.component.css']
 })
-export class ChatBoxComponent implements OnInit {
+export class ChatBoxComponent implements OnInit, AfterViewChecked {
   @Input() nickname: string;
+  @ViewChild('messageList', {static: false}) myScrollContainer: ElementRef;
+  scrollContainer: HTMLElement;
   socket: any;
   messageList: Array<MyMessage> = new Array();
   message: MyMessage = new MyMessage();
-  // nickname = 'Kamil';
   contents: string;
+  reg1 = new RegExp('\\s{2,}'); // podwójne znaki białe
+  reg2 = new RegExp('^\\s'); // znaki białe na początku
+  reg3 = new RegExp('\\s{2,}$'); // znaki białe na końcu
+  reg4: boolean; // pusta wiadomość
   constructor() { }
 
   ngOnInit(): void {
-    // this.message.contents = '';
     this.setupSocketConnection();
+  }
+
+  ngAfterViewChecked(): void {
+    this.scrollToElement();
   }
 
   setupSocketConnection(): void {
@@ -33,14 +40,20 @@ export class ChatBoxComponent implements OnInit {
   }
 
   SendMessage(): void {
-    this.message.nickname = this.nickname;
-    this.message.contents = this.contents;
-    // console.log(this.message);
-    this.socket.emit('message', this.message);
-    // this.messageList.push(this.message);
-    this.contents = '';
+    if (this.contents === '') { this.reg4 = true; } else { this.reg4 = false; }
+    if (this.reg1.test(this.contents) || this.reg2.test(this.contents) || this.reg3.test(this.contents) || this.reg4) {}
+    else {
+      this.message.nickname = this.nickname;
+      this.message.contents = this.contents;
+      this.socket.emit('message', this.message);
+      this.contents = '';
+    }
   }
 
+  scrollToElement(): void {
+    this.scrollContainer = document.getElementById('messageList');
+    this.scrollContainer.scrollTop = this.scrollContainer.scrollHeight;
+  }
 }
 
 export class MyMessage {
